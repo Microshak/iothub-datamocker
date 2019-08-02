80218ffa6a8b860ecd62df25255cb5b79eb5680d
Microshak <roshak@hotmail.com>
2019-08-01 17:52:13 -0700

    2

--- IoTHubDataLoader.py
+++ IoTHubDataLoader.py
@@ -1,23 +1,32 @@
 from D2CMsgSender import IoTDataMsgSender
 import time 
 from BuildData import BuildData
-connectionString = 'HostName=iothub-yhb5s.azure-devices.net;DeviceId=mikesdevice;SharedAccessKey=t0RiwgNTjND4MUoJYEm4toqXc4GaXSzcNZlR8IkmFTY='
-MessagesToSend = 10000
+connectionString = 'HostName=iothub-k3s3b.azure-devices.net;DeviceId=NewMikesDevice;SharedAccessKey=BY5HdQTm5fKaJF0RhkMZq6N6iMqfFyrd/yq87onJTMo='
+connectionString2 = 'HostName=iothub-k3s3b.azure-devices.net;DeviceId=MikesNewDevice2;SharedAccessKey=Kwnl/JM88Xs70FPI341MK3iDRCohI5lxl3xO8zdztLE='
+MessagesToSend = 1000000
 
 
 if __name__ == '__main__':  
     
-    IoTHubMsgSender = IoTDataMsgSender(connectionString)
+   
     buildData = BuildData()
     template = buildData.loadTemplate("template.json")
+    IoTHubMsgSender = IoTDataMsgSender(connectionString)
+    m1 = IoTDataMsgSender(connectionString)
+    m2 = IoTDataMsgSender(connectionString2)
     x = 0
     while(x < MessagesToSend):
         x = x+1
+        if x % 2 == 0:
+            IoTHubMsgSender = m2
+        else:
+            IoTHubMsgSender = m1
+
         message = buildData.getDistribution(template)
         sasToken =  IoTHubMsgSender.buildIoTHubSasToken()
         print(IoTHubMsgSender.sendD2CMsg(message,sasToken))
         print(message)
-        time.sleep(1)
+        time.sleep(.2)
 
 
 
